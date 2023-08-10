897173b094f81c40ef657a4a3c572dcfdddc7a2d
Jake Wright <jaketwright0@gmail.com>
2023-08-10 18:01:15 +0100

    add dotenv rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,48 @@
 require 'open-uri'
 require 'json'
+require 'nokogiri'
+poster_urls = []
+titles = []
+overviews = []
+# movies = {
+#   title:
+#   poster_url:
+#   overviews:
+# }
+url = 'https://www.imdb.com/list/ls566160570/'
+html = URI.open(url).read
+doc = Nokogiri::HTML.parse(html)
+url_posters = "https://letterboxd.com/bfi/list/sight-and-sounds-greatest-films-of-all-time/"
+html_posters = URI.open(url_posters).read
+doc_posters = Nokogiri::HTML.parse(html_posters)
 
-url = "https://api.themoviedb.org/3/movie/top_rated"
 
-puts "Cleaning the database"
+
+doc.search('.lister-item-image').each do |p|
+ p poster_urls << p.css("img").attribute("src").value
+end
+doc.search('.lister-item-content').each do |p|
+  overviews << p.css("p")[1].inner_text
+end
+doc.search('.lister-item-content').css('.lister-item-header').each do |p|
+  titles << p.css('a').inner_text
+end
+
+
+movies_array = [titles, overviews, poster_urls].transpose.each { |x, y, z| p [x, y, z] }
+
+puts "clearing the database"
+
 Movie.destroy_all
 
-puts "Adding movies..."
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "Adding movies!"
+movies_array.each do |movie|
+  new_movie = Movie.new(
+    title: movie[0],
+    overview: movie[1],
+    poster_url: movie[2]
+  )
+  new_movie.save
+end
 
 puts "Finished"
